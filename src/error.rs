0ee8f211c076f4210c282aaccc79692eae42d28e
Freezi <freeziftw@gmail.com>
2021-11-19 20:01:26 +0100

    Add wrap external errors

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,7 @@
 use std::{result, fmt, io};
+use std::num::TryFromIntError;
+use std::string::FromUtf8Error;
+
 pub type Result<T> = result::Result<T, Error>;
 
 #[derive(fmt::Debug)]
@@ -6,4 +9,25 @@ pub enum Error {
     InvalidVersion,
     InvalidCommand,
     InvalidAddrType,
+    Io(io::Error),
+    Utf8(FromUtf8Error),
+    Int(TryFromIntError),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<FromUtf8Error> for Error {
+    fn from(err: FromUtf8Error) -> Self {
+        Self::Utf8(err)
+    }
+}
+
+impl From<TryFromIntError> for Error {
+    fn from(err: TryFromIntError) -> Self {
+        Self::Int(err)
+    }
 }
